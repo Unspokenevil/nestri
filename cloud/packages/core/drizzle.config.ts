73a6d9546a2da183f433f3264aeb5ba3ba8e1554
Wanjohi <elviswanjohi47@gmail.com>
2025-09-20 06:54:57 +0300

    feat: Push schema to remote db

--- cloud/packages/core/drizzle.config.ts
+++ cloud/packages/core/drizzle.config.ts
@@ -1,19 +1,12 @@
 import { Resource } from "sst";
 import { defineConfig } from "drizzle-kit";
 
-const connection = {
-  user: Resource.Database.user,
-  password: Resource.Database.password,
-  host: Resource.Database.host,
-};
-
 export default defineConfig({
   verbose: true,
-  strict: true,
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: `postgres://${connection.user}:${connection.password}@${connection.host}/nestri`,
+    url: `postgres://${Resource.Database.user}:${Resource.Database.password}@${Resource.Database.host}/${Resource.Database.name}?sslmode=require`,
   },
   schema: "./src/**/*.sql.ts",
 });
