c9a0e6ee293403fb659d9332bfd888f0b559942e
DatCaptainHorse <DatCaptainHorse@users.noreply.github.com>
2025-11-11 11:45:00 +0200

    fix: Specify MangoHud config for gamescope

--- packages/scripts/envs.sh
+++ packages/scripts/envs.sh
@@ -13,3 +13,6 @@ export PROTON_NO_FSYNC=1
 # Make gstreamer GL elements work without display output (NVIDIA issue..)
 export GST_GL_API=gles2
 export GST_GL_WINDOW=surfaceless
+
+# Gamescope does not respect MangoHud default config location
+export MANGOHUD_CONFIGFILE=${NESTRI_HOME}/.config/MangoHud/MangoHud.conf
